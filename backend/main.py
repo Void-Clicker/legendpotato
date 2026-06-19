7c2c911cdeba1ff173cdc03280e5e293c8b59250
Quantum <anim-awuahm@outlook.com>
2026-06-19 17:39:08 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 )
 
 SUPABASE_URL = "https://gzcbnuxuraoavywfouhg.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI..."   # ← YOUR FULL ANON KEY HERE
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6Y2JudXh1cmFvYXZ5d2ZvdWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MTA0NTUsImV4cCI6MjA5NzI4NjQ1NX0.kkg9kL7jq4EzIap4i0OFkLdqQPG-geyObe_Evd-cFVk"   # ← YOUR FULL ANON KEY HERE
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
